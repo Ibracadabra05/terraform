caaab4030ca7b3ec811af3ad7bcb61e07ea6de25
Ibrahim <ibrahim.hamisu@berkeley.edu>
2019-06-23 13:48:47 -0500

    Name instance properly & add name tag

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ provider "aws" {
   version = "~> 2.15"
 }
 
-resource "aws_instance" "ibrah-useast1a" {
+resource "aws_instance" "ibrah-useast-1a" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+
+  tags {
+    Name = "ibrah-useast-1a"
+  }
 }
