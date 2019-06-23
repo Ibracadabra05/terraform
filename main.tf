8d5713eda849307e9a2ade82c4ae137a6b025ea8
Ibrahim <ibrahim.hamisu@berkeley.edu>
2019-06-23 15:07:18 -0500

    Start web server on boot

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ resource "aws_instance" "ibrah-useast-1a" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
     Name = "ibrah-useast-1a"
   }
