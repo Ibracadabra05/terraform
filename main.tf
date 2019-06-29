e40635957ae42facfd2e2bad85ae83440076bd2b
Ibrahim <ibrahim.hamisu@berkeley.edu>
2019-06-28 21:42:16 -0500

    Make port configurable & output public_ip

--- main.tf
+++ main.tf
@@ -3,15 +3,20 @@ provider "aws" {
   version = "~> 2.15"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
 resource "aws_instance" "ibrah-useast-1a" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
+  ami                    = "ami-40d28157"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags = {
@@ -23,9 +28,13 @@ resource "aws_security_group" "webserver" {
   name = "webserver"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.ibrah-useast-1a.public_ip}"
 }
